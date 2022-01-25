d1082d4605daea34477640850dcaa683459fbca1
Kacong Arip <arifsaqa@gmail.com>
2022-01-26 05:26:35 +0700

    kotlin app init

---
+++ app/src/main/java/com/example/dicoding/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.dicoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private  lateinit var editWidth : EditText
+    private  lateinit var editLength : EditText
+    private  lateinit var editHeight : EditText
+    private  lateinit var btnCalculate : Button
+    private  lateinit var tvResult : TextView
+
+    companion object {
+        private  const val STATE_RESULT = "state_result"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editHeight = findViewById(R.id.edt_height)
+        editWidth = findViewById(R.id.edt_width)
+        editLength = findViewById(R.id.edit_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        if (savedInstanceState != null) {
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvResult.text = result
+        }
+        btnCalculate.setOnClickListener(this)
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+
+    override fun onClick(p0: View?) {
+        if (p0?.id == R.id.btn_calculate) {
+            val inputLength = editLength.text.toString().trim()
+            val inputHeight = editHeight.text.toString().trim()
+            val inputWidth = editWidth.text.toString().trim()
+
+
+            var isEmptyFields = false
+
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                editLength.error = "Field ini tidak boleh kosong"
+            }
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                editWidth.error = "Field ini tidak boleh kosong"
+            }
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true;
+                editHeight.error = "Field ini tidak boleh kosong"
+            }
+
+            if (!isEmptyFields) {
+                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+                tvResult.text = volume.toString()
+            }
+        }
+    }
+
+}
